a133a7bfc7c20eaed44364157a5f598fb35b0a4d
Tiyo <tiyosheng@gmail.com>
2025-02-20 19:09:41 +0800

    remove console

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  build: {
-    minify: 'terser', // 切换到 terser 进行压缩
-    terserOptions: {
-      compress: {
-        drop_console: true, // 生产环境去除所有 console.* 调用
-      },
-    },
+  esbuild: {
+    drop: ['console', 'debugger'], // 同时去除 console 和 debugger
   },
 })
